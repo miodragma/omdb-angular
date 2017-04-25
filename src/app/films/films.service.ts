1c40f84288debe7ccf2ed97d3cefed4e006cf3af
Mio86 <miodrag.maric86@gmail.com>
2017-04-25 09:16:00 +0200

    ready

--- src/app/films/films.service.ts
+++ src/app/films/films.service.ts
@@ -8,18 +8,18 @@ export class FilmsService {
   constructor(private http: Http) { }
 
   getFilms(titleValue: any){
-    return this.http.get(`http://www.omdbapi.com/?s=${titleValue}`).map((response: Response) => response.json())
+    return this.http.get(`https://www.omdbapi.com/?s=${titleValue}`).map((response: Response) => response.json())
   }
 
   getNextFilms(titleValue: any, pageValue: number){
-    return this.http.get(`http://www.omdbapi.com/?s=${titleValue}&page=${pageValue}`).map((response: Response) => response.json())
+    return this.http.get(`https://www.omdbapi.com/?s=${titleValue}&page=${pageValue}`).map((response: Response) => response.json())
   }
 
   getDetails(value: any){
-    return this.http.get(`http://www.omdbapi.com/?i=${value}`).map((response: Response) => response.json())
+    return this.http.get(`https://www.omdbapi.com/?i=${value}`).map((response: Response) => response.json())
   }
 
   getSeasons(idIndex: any, index: any){
-    return this.http.get(`http://www.omdbapi.com/?i=${idIndex}&Season=${index}`).map((response: Response) => response.json())
+    return this.http.get(`https://www.omdbapi.com/?i=${idIndex}&Season=${index}`).map((response: Response) => response.json())
   }
 }
