e1c0af119ad3041414050acf57cf236bbbb947f0
Mio86 <miodrag.maric86@gmail.com>
2017-04-25 09:59:08 +0200

    ready

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,9 @@ import { FILMS_ROUTES } from "./films/films.routes";
 import { CompareFilmsComponent } from "./compare-films/compare-films.component";
 
 const APP_ROUTES_PROVIDERS: Routes = [
-  { path: '', redirectTo: '/films', pathMatch: 'full' },
   { path: 'films', component: FilmsComponent, children: FILMS_ROUTES },
-  { path: 'compare-films', component: CompareFilmsComponent }
+  { path: 'compare-films', component: CompareFilmsComponent },
+  { path: '**', redirectTo: '/films', pathMatch: 'full' }
 ];
 
 export const APP_ROUTES = RouterModule.forRoot(APP_ROUTES_PROVIDERS);
